e4eede482e15dd1c82c65bb1a1e02396d7ee872e
Brenson <gobrenson28@gmail.com>
2026-02-06 19:35:00 +0800

    creating register & profile view

--- inventory/apps/accounts/views.py
+++ inventory/apps/accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import ProfileForm, RegisterForm
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect("dashboad:index")
+    
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Account created successfully")
+            return redirect("accounts:profile")
+    else:
+        form = RegisterForm()
+    
+    return render(request, "accounts/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    profile_obj = request.user.profile
+
+    if request == "POST":
+        form = ProfileForm(request.POST, instance=profile_obj)
+        if form.is_valid():
+            messages.success(request, "Profile Update")
+            return redirect("accounts:profile")
+    else:
+        form = ProfileForm(instance=profile_obj)
+
+    return render(request, "accounts/profile.html", {"form": form})
+    
