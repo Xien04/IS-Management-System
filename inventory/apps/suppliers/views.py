31b064143c5c094707ee7249611c492b322d4186
Brenson <gobrenson28@gmail.com>
2026-02-06 13:36:39 +0800

    creating views functions

--- inventory/apps/suppliers/views.py
+++ inventory/apps/suppliers/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
+from .forms import SupplierForm
+from .models import Supplier
 # Create your views here.
+
+class SupplierListView(LoginRequiredMixin, ListView):
+    model = Supplier
+    template_name = "suppliers/list.html"
+    context_object_name = "suppliers"
+    paginate_by = 10
+
+class SupplierDetailView(LoginRequiredMixin, DetailView):
+    model = Supplier
+    template_name = "suppliers/detail.html"
+    context_object_name = "supplier"
+
+class SupplierCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Supplier
+    form_class = SupplierForm
+    template_name = "suppliers/form.html"
+    success_url = reverse_lazy("suppliers:list")
+    success_message = "Supplier created successfully"
+
+class SupplierDeleteView(LoginRequiredMixin, DeleteView):
+    model = Supplier
+    template_name = "suppliers/confirm_delete.html"
+    success_url = reverse_lazy("suppliers:list")
+    context_object_name = "supplier"
