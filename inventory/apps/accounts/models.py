dc1afe03ba8601ce8545c97b6c23ed68110d8443
Brenson <gobrenson28@gmail.com>
2026-02-06 19:18:16 +0800

    creating profile models

--- inventory/apps/accounts/models.py
+++ inventory/apps/accounts/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=32, blank=True)
+    role = models.CharField(max_length=64, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.user.username} Profile"
