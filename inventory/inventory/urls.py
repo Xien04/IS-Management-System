5a218dfb6f7633c54d9a379864220f64bbb932fe
Brenson <gobrenson28@gmail.com>
2026-02-06 10:21:54 +0800

    add dashboard urls

--- inventory/inventory/urls.py
+++ inventory/inventory/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("apps.dashboard.url"))
+    path("", include("apps.dashboard.urls"))
 ]
