ed581fd88d51e13397a8d28b7b4114f5061b23c7
Brenson <gobrenson28@gmail.com>
2026-02-06 13:44:13 +0800

    creating supplier urls

---
+++ inventory/apps/suppliers/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "suppliers"
+
+urlpatters = [
+    path("", views.SupplierListView.as_view(), name="list"),
+    path("create/", views.SupplierCreateView.as_view(), name="create"),
+    path("<int:pk>/", views.SupplierDetailView.as_view(), name="detail"),
+    path("<int:pk>/edit/", views.SupplierUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.SupplierDeleteView.as_view(), name="delete"),
+]
