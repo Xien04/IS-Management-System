4bbd939ecc98550bbe5bc1154a519e237e1bb604
Brenson <gobrenson28@gmail.com>
2026-02-15 22:15:39 +0800

    adding ready noqa:F401

--- inventory/apps/products/apps.py
+++ inventory/apps/products/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.products'
+
+    def ready(self) -> None:
+        from . import signals # noqa: F401
