51fe87ecca87b7edd61bca2b54d62bc70d81a06b
Brenson <gobrenson28@gmail.com>
2026-02-16 21:27:56 +0800

    Inventory models update

--- inventory/apps/inventory/models.py
+++ inventory/apps/inventory/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class InventoryItem(models.Model):
+    product = models.OneToOneField(
+        "products.Product",
+        on_delete = models.CASCADE,
+        related_name = "inventory",
+    )
+    quantity_on_hand = models.IntegerField(default=0)
+    reorder_level = models.IntegerField(default=0)
+    location = models.CharField(max_length=120, blank=True)
+    updated_at = models.DateTimeField(auto_no=True)
+
+    class Meta:
+        ordering = ["product_name"]
+
+
+    @property
+    def low_stock(self) -> bool:
+        return self.quantity_on_hand <= self.reorder_level
