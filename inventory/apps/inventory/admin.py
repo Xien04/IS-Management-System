4b2b6eb6005ef35251916c0c8d24fa89ba670007
Brenson <gobrenson28@gmail.com>
2026-02-16 21:27:14 +0800

    Register inventory model

--- inventory/apps/inventory/admin.py
+++ inventory/apps/inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import InventoryItem
+
+@admin.register(InventoryItem)
+class InventoryItemAdmin(admin.ModelAdmin):
+    list_display = ("product", "quantity_on_hand", "reorder_level", "location", "updated_at")
+    list_filter = ("location")
+    search_field = ("product_name", "product_sku")
