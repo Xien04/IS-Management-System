5ce6b9e29d7853b17947585779192fcc69235045
Brenson <gobrenson28@gmail.com>
2026-02-17 15:50:11 +0800

    Admin register

--- inventory/apps/sales/admin.py
+++ inventory/apps/sales/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sale, SaleItem
+
+class SaleItemInLine(admin.TabularInline):
+    model = SaleItem
+    extra = 0
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ("id", "customeer_name", "sale_date")
+    search_fields = ("customer_name")
+    inlines = [SaleItemInLine]
+
+@admin.register(SaleItem)
+class SaleItemAdmin(admin.ModelAdmin):
+    list_display = ("sale", "product", "quantity", "unit_price")
+    search_fields = ("sale__id", "product__name", "product__sku")
